5b3cd9bb26265f51eee107bafae4823dd08a0599
NadiasCodes <De.nadia@outlook.com>
2024-04-09 13:25:55 +0200

    created a functional stopwatch with start, stop and reset options

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -1,26 +1,49 @@
-import React, { useRef, useState, useEffect } from "react";
-
-
-
+import React, {useState,useEffect,useRef } from "react";
 
 const StopWatch = () => {
+  const [isRunning, setIsRunning] = useState(false); //checking if the stopwatch is currently running
+  const [timeElapsed, setTimeElapsed] = useState(0); // keep a track of how much time it elapsed
+  const intervalIdRef = useRef(null); // We are working with intervals and we need to clear it if we are not using it
+  const startTimeRef = useRef(0); // When we start the stopwatch, we will have to get the current time.
 
-const [isRunning, setIsRunning] = useState(false); //checking if the stopwatch is currently running
-const [timeElapsed, setTimeElapsed] = useState(0); // keep a track of how much time it elapsed
-const intervalIdRef = useRef(null); // We are working with intervals and we need to clear it if we are not using it
-const startTimeRef = useRef(0); // When we start the stopwatch, we will have to get the current time.
+  useEffect(() => {
+    if (isRunning) {
+      intervalIdRef.current = setInterval(() => {
+        setTimeElapsed(Date.now() - startTimeRef.current);
+      }, 10);
+    }
 
-useEffect(()=>{
+    return () => {
+      clearInterval(intervalIdRef.current);
+    };
+  }, [isRunning]);
 
-}, [isRunning])
-  const handleStart = () => {};
+  const handleStart = () => {
+    setIsRunning(true);
+    startTimeRef.current = Date.now() - timeElapsed;
+  };
 
-  const handleStop = () => {};
+  const handleStop = () => {
+    setIsRunning(false);
+  };
 
-  const handleReset = () => {};
+  const handleReset = () => {
+    setTimeElapsed(0);
+    setIsRunning(false);
+  };
 
   const formatTime = () => {
-    return "00:00:00";
+    let hours = Math.floor(timeElapsed / (1000 * 60 * 60));
+    let minutes = Math.floor(timeElapsed / (1000 * 60) % 60);
+    let seconds = Math.floor(timeElapsed / (1000) % 60);
+    let milliseconds = Math.floor((timeElapsed % 1000) / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    return `${hours}:${minutes}:${seconds}:${milliseconds}`;
   };
 
   return (
